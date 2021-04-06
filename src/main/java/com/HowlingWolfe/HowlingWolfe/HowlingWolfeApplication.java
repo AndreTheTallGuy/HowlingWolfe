8453283e751c7ec0d424b06fd1a7330f1a54db55
Andre Entrekin <andre.entrekin@gmail.com>
2021-04-06 12:54:37 -0500

    added backend

---
+++ src/main/java/com/HowlingWolfe/HowlingWolfe/HowlingWolfeApplication.java
@@ -0,0 +1,13 @@
+package com.HowlingWolfe.HowlingWolfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowlingWolfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowlingWolfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HowlingWolfe/HowlingWolfe/HowlingWolfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HowlingWolfe.HowlingWolfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowlingWolfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
