c9e15ded8ca6c9f8d1d21366a773813f7e427aa1
Andre Entrekin <andre.entrekin@gmail.com>
2021-04-14 13:31:44 -0500

    deployment

--- src/main/java/com/HowlingWolfe/HowlingWolfe/HowlingWolfeApplication.java
+++ src/main/java/com/HowlingWolfe/HowlingWolfe/HowlingWolfeApplication.java
@@ -1,6 +1,5 @@
 package com.HowlingWolfe.HowlingWolfe;
 
-import com.HowlingWolfe.HowlingWolfe.email.SendEmail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
