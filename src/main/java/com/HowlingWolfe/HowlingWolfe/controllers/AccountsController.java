33784a00cae1152cf8e1b54f61b5bdc6ca841feb
Andre Entrekin <andre.entrekin@gmail.com>
2021-07-23 11:19:16 -0500

    cors update

--- src/main/java/com/HowlingWolfe/HowlingWolfe/controllers/AccountsController.java
+++ src/main/java/com/HowlingWolfe/HowlingWolfe/controllers/AccountsController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "https://howlingwolfe.herokuapp.com")
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "/accounts")
 public class AccountsController {
 
